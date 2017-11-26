7b53edbb9b1f9ecce3ac36187c7b0f31217f3f60
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-11-26 20:46:19 +0700

    pending
    access options

--- inc/access_func.php
+++ inc/access_func.php
@@ -7,13 +7,15 @@ if(!isset($_SESSION)){
 	session_start();
 }
 
+require_once("../../inc/db_connect.php");
 require_once("../../inc/init_response_func.php");
 
 $__ACCESS_LOGGEDIN = 0;
 $__ACCESS_VERIFIED = 1;
+$__ACCESS_OWNER = 2;
 $__ACCESS_ADMIN = 9;
 
-function access_check($prefix, $previlege = 0, $DataRequired = false){
+function access_check($prefix, $previlege = 0, $DataRequired = false, $options = []){
 	if(!isset($_SESSION['verified'])){
 		reject($prefix, "99", "Login gon ai sus!!!");
 	}
@@ -22,6 +24,24 @@ function access_check($prefix, $previlege = 0, $DataRequired = false){
 	}
 	if($DataRequired && !isset($_POST['data'])){
 		reject($prefix, "04", "No data KUYKUYKUYKUY!!!");
+	}
+
+	if($_SESSION['verified'] < $__ACCESS_ADMIN){
+		foreach($options as $option) {
+			switch($option){
+				case "self":
+					if($_SESSION['member_id'] != $previlege){
+						reject($prefix, "90", "Access denied, eiei olo.");
+					}
+					break;
+				case "owner":
+					# code...
+					break;
+				default:
+					# code...
+					break;
+			}
+		}
 	}
 }
 
