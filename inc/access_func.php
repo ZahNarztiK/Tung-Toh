cfc5e94346704fc2c68fc2cba44986e7fb31bcea
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-11-26 22:44:25 +0700

    Revert "pending"
    This reverts commit 7b53edbb9b1f9ecce3ac36187c7b0f31217f3f60.

--- inc/access_func.php
+++ inc/access_func.php
@@ -7,15 +7,13 @@ if(!isset($_SESSION)){
 	session_start();
 }
 
-require_once("../../inc/db_connect.php");
 require_once("../../inc/init_response_func.php");
 
 $__ACCESS_LOGGEDIN = 0;
 $__ACCESS_VERIFIED = 1;
-$__ACCESS_OWNER = 2;
 $__ACCESS_ADMIN = 9;
 
-function access_check($prefix, $previlege = 0, $DataRequired = false, $options = []){
+function access_check($prefix, $previlege = 0, $DataRequired = false){
 	if(!isset($_SESSION['verified'])){
 		reject($prefix, "99", "Login gon ai sus!!!");
 	}
@@ -24,24 +22,6 @@ function access_check($prefix, $previlege = 0, $DataRequired = false, $options =
 	}
 	if($DataRequired && !isset($_POST['data'])){
 		reject($prefix, "04", "No data KUYKUYKUYKUY!!!");
-	}
-
-	if($_SESSION['verified'] < $__ACCESS_ADMIN){
-		foreach($options as $option) {
-			switch($option){
-				case "self":
-					if($_SESSION['member_id'] != $previlege){
-						reject($prefix, "90", "Access denied, eiei olo.");
-					}
-					break;
-				case "owner":
-					# code...
-					break;
-				default:
-					# code...
-					break;
-			}
-		}
 	}
 }
 
