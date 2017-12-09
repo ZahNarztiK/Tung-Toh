530093355ed0a765a5fa6335a8157108f7e99c04
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-12-10 04:32:29 +0700

    always recheck privilege

--- inc/basic/access_func.php
+++ inc/basic/access_func.php
@@ -14,9 +14,26 @@ $GLOBALS['ACCESS_CONSTANT'] = [
 ];
 
 function access_check($prefix, $previlege = 0, $DataRequired = false){
-	if(!isset($_SESSION['verified'])){
+	if(!isset($_SESSION['member_id'])){
 		reject($prefix, $GLOBALS['RESPONSE_ERROR_CODE']['AC_LOGGED'], "Login gon ai sus!!!");
 	}
+
+	try{
+		global $DB_PDO;
+
+		$stmt = $DB_PDO->prepare("SELECT verified FROM member WHERE member_id = :member_id LIMIT 1");
+		$stmt->bindParam(':member_id', $_SESSION['member_id'], PDO::PARAM_INT);
+		$stmt->execute();
+		
+		if($stmt->rowCount() == 0){
+			reject($prefix, $GLOBALS['RESPONSE_ERROR_CODE']['DB_NODATA'], "User not found.");
+		}
+		$_SESSION['verified'] = $stmt->fetchColumn();
+	}
+	catch(PDOException $e){
+		reject($prefix, $GLOBALS['RESPONSE_ERROR_CODE']['PDO'], $e->getMessage());
+	}
+	
 	if($_SESSION['verified'] < $previlege){
 		reject($prefix, $GLOBALS['RESPONSE_ERROR_CODE']['AC_LOW'], "Access denied, eiei olo.");
 	}
